236d3d11b99a3b18194f2a79849e503f0805e0a3
Atharv7313 <137501939+Atharv7313@users.noreply.github.com>
2025-06-16 20:56:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const server=app.listen(process.env.PORT,()=>{
 const io = socket(server, {
     cors: {
         // origin: "http://localhost:3000",
-        origin: "https://chit-chat-app-mu.vercel.app/login",
+        origin: "https://chit-chat-app-mu.vercel.app",
         credentials: true,
     },
 });
